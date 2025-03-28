c848c4faa86da52a328852b914bfb6618152911f
Lenny0939 <lennyescott@gmail.com>
2025-03-29 09:36:36 +1100

    movement

--- flake.nix
+++ flake.nix
@@ -1,173 +1,35 @@
 {
-  description = "bevy flake";
+  description = "Rust flake with nightly";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    crane.url = "github:ipetkov/crane";
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-  outputs =
-    inputs@{
-      flake-parts,
-      fenix,
-      crane,
-      devshell,
-      treefmt-nix,
-      nixpkgs,
-      ...
-    }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
-      imports = [
-        devshell.flakeModule
-        treefmt-nix.flakeModule
-      ];
-      perSystem =
-        {
-          system,
-          config,
-          lib,
-          ...
-        }:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ fenix.overlays.default ];
-          };
 
-          packages = {
-            default = packages.game;
-            game = craneLib.buildPackage build-attrs // {
-              meta.mainProgram = "game";
-            };
-          };
-
-          toolchain =
-            with fenix.packages.${system};
-            combine [
-              latest.rustc
-              latest.cargo
-              latest.clippy
-              latest.rust-analysis
-              latest.rust-src
-              latest.rustfmt
-            ];
-
-          craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
-
-          build-deps = with pkgs; [
-						pkg-config 
-						mold 
-						clang 
-						udev 
-						alsa-lib 
-						vulkan-loader
-
-						xorg.libX11
-						xorg.libXcursor
-						xorg.libXrandr 
-						xorg.libXi
-
-						libxkbcommon
-						wayland
-					];
-
-          unfilteredRoot = ./.; # The original, unfiltered source
-
-          src = lib.fileset.toSource {
-            root = unfilteredRoot;
-            fileset = lib.fileset.unions [
-              # Default files from crane (Rust and cargo files)
-              (craneLib.fileset.commonCargoSources unfilteredRoot)
-              # Keep assets
-              (lib.fileset.maybeMissing ./assets)
-            ];
-          };
-
-          build-attrs = {
-            inherit src;
-            buildInputs = build-deps;
-          };
-
-          deps-only = craneLib.buildDepsOnly ({ } // build-attrs);
-
-          checks = {
-            clippy = craneLib.cargoClippy (
-              {
-                cargoArtifacts = deps-only;
-                cargoClippyExtraArgs = "--all-features -- --deny warnings";
-              }
-              // build-attrs
-            );
-
-            rust-fmt = craneLib.cargoFmt ({ inherit src; } // build-attrs);
-
-            rust-tests = craneLib.cargoNextest (
-              {
-                cargoArtifacts = deps-only;
-                partitions = 1;
-                partitionType = "count";
-              }
-              // build-attrs
-            );
-          };
-
-        in
-        {
-          inherit checks packages;
-          treefmt = {
-            programs = {
-              nixfmt-rfc-style.enable = true;
-              statix.enable = true;
-            };
-            flakeFormatter = true;
-            projectRootFile = "flake.nix";
-          };
-
-          devshells.default = {
-            packages =
-              with pkgs;
-              [
-                config.treefmt.build.wrapper
-                nixfmt-rfc-style
-                statix
-                # rust
-                gcc # required for clap
-                toolchain
-              ]
-              ++ build-deps;
-            env = [
-              {
-                name = "LD_LIBRARY_PATH";
-                value = "${pkgs.lib.makeLibraryPath build-deps}";
-              }
-            ];
-          };
-
+  outputs = { self, flake-utils, rust-overlay, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = (import nixpkgs) {
+          inherit system overlays;
         };
-    };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell rec {
+          nativeBuildInputs = [
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-analyzer" "clippy" "rust-src" ];
+            })
+          ];
+          buildInputs = [
+            udev alsa-lib vulkan-loader
+            xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr
+            libxkbcommon wayland pkg-config
+          ];
+          LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+        };
+      }
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,77 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .insert_resource(Time::<Fixed>::from_hz(64.0))
+        .add_systems(FixedUpdate, player_movement)
+        .add_systems(Update, (jump, smooth_movement))
+        .run();
+}
+
+const MAX_SPEED: f32 = 3000.0;
+const MIN_SPEED: f32 = 1500.0;
+const DEFAULT_JUMP: f32 = 1500.0;
+const GRAVITY: f32 = 4000.0;
+
+#[derive(Component)]
+struct PlayerState {
+    y_pos: f32,
+    velocity: f32,
+}
+
+#[derive(Component)]
+struct Player;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+    let velocity = 0.0;
+    commands.spawn((
+        Sprite::from_image(asset_server.load("dof.png")),
+        Transform::from_xyz(0., 0., 0.),
+        PlayerState { velocity, y_pos: 0.0 },
+        Player,
+    ));
+}
+
+/// The sprite is animated by changing its translation depending on the time that has passed since
+/// the last frame.
+fn player_movement(
+    time: Res<Time<Fixed>>,
+    mut sprite_position: Query<&mut PlayerState, With<Player>>,
+) {
+    for mut player_state in &mut sprite_position {
+        player_state.velocity -= GRAVITY * time.delta_secs();
+        player_state.y_pos += player_state.velocity * time.delta_secs();
+    }
+}
+
+fn smooth_movement(
+    time: Res<Time<Fixed>>,
+    mut sprite_position: Query<(&mut Transform, &mut PlayerState), With<Player>>,
+) {
+    for (mut transform, player_state) in &mut sprite_position {
+        let a = time.overstep_fraction();
+        //let a = time.delta_secs();
+        let future_position = player_state.y_pos
+            + player_state.velocity * time.delta_secs();
+        transform.translation.y = player_state.y_pos.lerp(future_position, a);
+        //transform.translation.y = player_state.y_pos;
+    }
+}
+
+fn jump(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut player_state: Query<&mut PlayerState, With<Player>>
+) {
+    if keyboard_input.just_pressed(KeyCode::Space) {
+        for mut player_state in &mut player_state {
+            match player_state.velocity {
+                ..0. => player_state.velocity = MIN_SPEED,
+                MAX_SPEED.. => player_state.velocity = MAX_SPEED,
+                _ => player_state.velocity += DEFAULT_JUMP
+            }
+        };
+    }
 }
