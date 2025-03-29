1f3488725a2f38836a00070a409e2bbb1f33208b
Lenny0939 <lennyescott@gmail.com>
2025-03-30 08:03:48 +1100

    basic spawning

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use bevy::{prelude::*, render::camera::ScalingMode};
+use rand::prelude::*;
+use std::time::Duration;
 
 fn main() {
     App::new()
@@ -9,7 +11,12 @@ fn main() {
             max_speed: 2000.0,
             min_speed: 1500.0,
         })
-        .add_systems(FixedUpdate, player_movement)
+        .insert_resource(PillarSpawnConfig {
+            timer: Timer::new(Duration::from_millis(2000), TimerMode::Repeating),
+        })
+        .insert_resource(PillarVelocity(1000.0))
+        .insert_resource(RngResource { rng: SmallRng::from_os_rng() })
+        .add_systems(FixedUpdate, (player_movement, spawn_pillars, pillar_movement))
         .add_systems(Update, (jump, smooth_movement))
         .run();
 }
@@ -21,12 +28,31 @@ struct MovementConfig {
 }
 
 #[derive(Resource)]
+struct RngResource { 
+    rng: SmallRng
+}
+
+#[derive(Resource)]
 struct GravityConfig(f32);
 
 #[derive(Component)]
-struct PlayerState {
-    y_pos: f32,
+struct PosState {
+    pos: f32,
     velocity: f32,
+}
+
+#[derive(Resource)]
+struct PillarVelocity(f32);
+
+#[derive(Component, PartialEq)]
+struct Player;
+
+#[derive(Component, PartialEq)]
+struct Pillar;
+
+#[derive(Resource)]
+struct PillarSpawnConfig {
+    timer: Timer,
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -43,12 +69,32 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // spawn the player
     commands.spawn((
         Sprite::from_image(asset_server.load("dof.png")),
-        Transform::from_xyz(0., 0., 0.),
-        PlayerState {
+        PosState {
             velocity,
-            y_pos: 540.0,
+            pos: 540.0,
         },
+        Player,
     ));
+}
+
+fn spawn_pillars(
+    mut commands: Commands,
+    mut pillar: ResMut<PillarSpawnConfig>,
+    asset_server: Res<AssetServer>,
+    pillar_velocity: Res<PillarVelocity>,
+    time: Res<Time<Fixed>>,
+    mut rng: ResMut<RngResource>,
+) {
+    pillar.timer.tick(time.delta());
+    let random_height = rng.rng.random_range(-540.0..540.0);
+    if pillar.timer.finished() {
+        commands.spawn((
+            Sprite::from_image(asset_server.load("dof.png")),
+            Transform::from_xyz(0.0, random_height, 0.0),
+            PosState { pos: 500.0, velocity: pillar_velocity.0 },
+            Pillar,
+        ));
+    };
 }
 
 /// The sprite is animated by changing its translation depending on the time that has passed since
@@ -56,29 +102,44 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 fn player_movement(
     time: Res<Time<Fixed>>,
     gravity: Res<GravityConfig>,
-    mut sprite_position: Query<&mut PlayerState>,
+    mut sprite_position: Query<&mut PosState, With<Player>>,
 ) {
     for mut player_state in &mut sprite_position {
         player_state.velocity -= gravity.0 * time.delta_secs();
-        player_state.y_pos += player_state.velocity * time.delta_secs();
+        player_state.pos += player_state.velocity * time.delta_secs();
+    }
+}
+
+fn pillar_movement(
+    time: Res<Time<Fixed>>,
+    velocity: Res<PillarVelocity>,
+    mut query: Query<&mut PosState, With<Pillar>>,
+) {
+    for mut pillar_state in &mut query {
+        pillar_state.pos -= velocity.0 * time.delta_secs();
     }
 }
 
 fn smooth_movement(
     time: Res<Time<Fixed>>,
-    mut sprite_position: Query<(&mut Transform, &mut PlayerState)>,
+    mut query: Query<(&mut Transform, &mut PosState, Option<&Player>, Option<&Pillar>)>,
 ) {
-    for (mut transform, state) in &mut sprite_position {
+    for (mut transform, state, player, pillar) in &mut query {
         let a = time.overstep_fraction();
-        let future_position = state.y_pos + state.velocity * time.delta_secs();
-        transform.translation.y = state.y_pos.lerp(future_position, a);
+        let future_position = state.pos + state.velocity * time.delta_secs();
+        if player.is_some() {
+            transform.translation.y = state.pos.lerp(future_position, a);
+        };
+        if pillar.is_some() {
+            transform.translation.x = state.pos.lerp(future_position, a);
+        }
     }
 }
 
 fn jump(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     movement: Res<MovementConfig>,
-    mut player_state: Query<&mut PlayerState>,
+    mut player_state: Query<&mut PosState, With<Player>>,
 ) {
     if keyboard_input.just_pressed(KeyCode::Space) {
         for mut player_state in &mut player_state {
