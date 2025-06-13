13dbf116b40a86d17ae317f3ac58fd4f2bb2b343
Lenny0939 <lennyescott@gmail.com>
2025-06-14 09:39:15 +1000

    add fps logging

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PresentMode};
 use flappydof::{collision::collision, pillar::{increment_pillar_timer, pillar_movement, pillar_timer, spawn_pillars}, player_movement::{check_jump, jump, player_movement}, setup, smooth_movement::smooth_movement, IsGameEnded, MovementConfig, RngResource, Score, pillar};
 use rand::prelude::*;
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use std::time::Duration;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                present_mode: PresentMode::AutoNoVsync,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugins(FrameTimeDiagnosticsPlugin::default())
+        .add_plugins(LogDiagnosticsPlugin::default())
         .add_systems(Startup, setup::setup)
         .insert_resource(IsGameEnded(false))
         .insert_resource(Score(0))
